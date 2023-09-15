4a607468225b508637e339b6b8e240bb6793bee7
Kimberly Rv <elliis.rv@gmail.com>
2023-09-14 18:46:48 -0600

    validateLinks function working correctly

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const functions = require('./functions')
 
 function mdLinks(path, options){
 let absolutePath = '';
+let returnValues = [];
 
 if (!functions.validatePath(path)) {
   return false;
 }
 if (functions.validateAbsolutePath(path)) {
-  console.log('Absolute Path is ' + path)
+  //console.log('Absolute Path is ' + path)
   absolutePath = path;
 } else {
-  console.log('Absolute Path is ' + functions.convertToAbsolutePath(path))
+  //console.log('Absolute Path is ' + functions.convertToAbsolutePath(path))
   absolutePath = functions.convertToAbsolutePath(path)
 };
 //console.log(absolutePath);
@@ -19,9 +20,16 @@ if (!functions.identifyFile(absolutePath)) {
   return false
 };
 if (functions.identifyFileExtension(absolutePath) === '.md') {
-  functions.getLinks(absolutePath);
+  returnValues.push(functions.getLinks(absolutePath));
 } else {
   return false
 };
+console.log(returnValues)
+return returnValues
 }
 mdLinks('example.md')
+
+
+module.exports = {
+  mdLinks: mdLinks
+}
---
+++ test/functions.spec.js
@@ -0,0 +1,67 @@
+const functions = require('../functions')
+
+describe('Test to validate absolute paths', () => {
+    it('Debería validar una ruta absoluta correctamente', () => {
+        const path = 'C:/Users/Kimberly/Documents/Laboratoria-Dev008/DEV008-md-links/example.md';
+        expect(functions.validateAbsolutePath(path)).toEqual(true);
+    });
+    it('Debería validar una ruta relativa correctamente', () => {
+        const path = 'testing_docs/DataLovers.md';
+        expect(functions.validateAbsolutePath(path)).toEqual(false);
+    });
+});
+
+describe('convertToAbsolutePath', () => {
+    it('Should return an absolute path from a relative path', () => {
+        const relativePath = 'example.md';
+        expect(functions.convertToAbsolutePath(relativePath)).toEqual(
+            'C:/Users/Kimberly/Documents/Laboratoria-Dev008/DEV008-md-links/example.md'
+        );
+    });
+});
+
+describe('identifyFileExtension', () => {
+    it('Should return the extension of a file', () => {
+        const path = 'testing_docs/DataLovers.md';
+        expect(functions.identifyFileExtension(path)).toEqual('.md');
+    });
+});
+
+describe('getLinks', () => {
+    it.only('Should return the links from a file', () => {
+        const path = 'example.md';
+        expect(functions.getLinks(path)).toEqual([
+            {
+                file: 'example.md',
+                href: 'https://www.instagram.com/',
+                text: 'Instagram '
+            },
+            {
+                file: 'example.md',
+                href: 'https://www.facebook.com/',
+                text: 'Facebook'
+            }
+        ]);
+    });
+});
+
+describe('validateLinks valida links', () => {
+    it('Debería validar el estatus de los links', () => {
+        const path = 'example.md';
+        functions.validateLinks(path).then((response) => {
+            expect(response).toEqual([{
+                href: 'https://www.instagram.com/',
+                text: 'Instagram',
+                file: 'example.md',
+                status: 200,
+                ok: 'ok'
+            }, {
+                href: 'https://www.facebook.com/',
+                text: 'Facebook',
+                file: 'example.md',
+                status: 200,
+                ok: 'ok'
+            }])
+        })
+    });
+});
